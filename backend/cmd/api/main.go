492e9b9bfa66445e6945f4937c921cd1223154b8
thanksleon <leon.kim.0509@gmail.com>
2021-10-15 23:54:17 -0400

    Update main.go

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -43,8 +43,8 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment(development|production)")
-	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://postgres:umzJ5AEqa@localhost/go_movies?sslmode=disable", "Postgres connection string")
-	//flag.StringVar(&cfg.jwt.secret,"jwt-secret", "2dce505d96a53c5768052ee90f3df2055657518dad489160df9913f66042e160", "secret")
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://postgres:"password"@localhost/go_movies?sslmode=disable", "Postgres connection string")
+	//flag.StringVar(&cfg.jwt.secret,"jwt-secret", "key", "secret")
 	flag.Parse()
 
 	//read jwt secret from env
